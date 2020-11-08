5f734fef86b300a3cb6b531993f9d49f19f61c9d
I_love_study <1450069615@qq.com>
2020-11-08 16:50:12 +0800

    删除了一些你用不到的文件

--- expand/black.py
+++
@@ -1,9 +0,0 @@
-import aiofiles
-
-with open('data/黑名单.txt','r',encoding = 'UTF-8') as f:
-    listing = [int(a.replace('\n','')) for a in f.readlines()]
-
-async def update():
-    global listing
-    async with aiofiles.open('data/黑名单.txt', 'w', encoding = 'UTF-8') as f:
-        await f.write("\n".join(str(q_id) for q_id in listing))
